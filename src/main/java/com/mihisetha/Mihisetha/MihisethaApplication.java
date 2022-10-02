9ce497489dcd2087cebea1c3d38035d429d8a44f
Dulana <dulanamanul123@gmail.com>
2022-10-02 13:04:44 +0530

    initial commit

---
+++ src/main/java/com/mihisetha/Mihisetha/MihisethaApplication.java
@@ -0,0 +1,13 @@
+package com.mihisetha.Mihisetha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MihisethaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MihisethaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mihisetha/Mihisetha/MihisethaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mihisetha.Mihisetha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MihisethaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
